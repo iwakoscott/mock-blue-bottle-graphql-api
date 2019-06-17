4a49ef20588393791ee9021428f9cb58626c7439
Scott Iwako <iwakoscott@gmail.com>
2019-06-17 08:58:39 -0700

    add interface properties to new implemented types

--- typeDefs.js
+++ typeDefs.js
@@ -16,10 +16,22 @@ const schema = gql`
   }
 
   type Blend implements Coffee {
+    id: ID!
+    name: String!
+    flavorNotes: [String!]!
+    unitPrice: Float!
+    unitWeight: Float!
+    imageURL: String!
     countries: [Country!]!
   }
 
   type SingleOrigin implements Coffee {
+    id: ID!
+    name: String!
+    flavorNotes: [String!]!
+    unitPrice: Float!
+    unitWeight: Float!
+    imageURL: String!
     country: Country!
   }
 `;
